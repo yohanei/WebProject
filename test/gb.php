15fca95b87a2863fd089887ec6e5c29d0100a995
P.S. Li <p.s.li19980101@gmail.com>
2017-05-29 01:19:31 +0800

    save 0529

--- test/gb.php
+++ test/gb.php
@@ -1,29 +1,8 @@
 <?php
+	
 	include("connectdb.php");
- 	$name=mysql_query("select 'Name' from 'guestbook'");
- 	$message=mysql_query("select 'Message' from 'guestbook'");
- 	$date=mysql_query("select 'Date' from 'guestbook'");
- 	$rows=mysql_query("select * from 'guestbook'");
-
- 	for($i=1;$i<=mysql_num_rows($rows);$i++){
- 		echo "
- 			<table>
- 				<tr>
-					<td><b>君の名は：</b></td>
-					<td>mysql_fetch_row($name)</td>
-				</tr>
-				<tr>
-					<td><b>我想說：</b></td>
-					<td>mysql_fetch_row($message)</td>
-				</tr>
-				<tr>
-					<td><b>時間：</b></td>
-					<td>mysql_fetch_row($date)</td>
-				</tr>
- 			</table>
- 			<br>
-  		";
- 	}
+ 	$sql = "SELECT * FROM `view_gb`";
+ 	$rows=mysql_query($sql);
 
  	while(list($a1, $a2, $a3)=mysql_fetch_row($rows)) {
  		echo "
